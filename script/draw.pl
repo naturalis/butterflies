ad7a69835b95df03448be4cf00535cd4640daefa
rvosa <rutgeraldo@gmail.com>
2016-12-11 20:54:52 +0100

    regenerated tree with collapsed clades and scale bar

--- script/draw.pl
+++ script/draw.pl
@@ -2,39 +2,35 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Data::Dumper;
 use Color::Spectrum;
 use Bio::Phylo::Treedrawer;
 use Bio::Phylo::IO qw'parse_tree';
 use Bio::Phylo::Util::Logger qw':simple :levels';
 
 # process command line arguments
-my ( $intree, $dir );
-my $genera    = 1;
+my ( $intree, $fossils, $taxa );
 my $verbosity = WARN;
-my $width     = 1000;
-my $height    = 4000;
-my $format    = 'nexus'; # of $intree
-my $shape     = 'rect';
+my $width     = 1500;
+my $height    = 2500;
+my $padding   = 40;
+my $shape     = 'curvy';
 my $mode      = 'phylo';
-my $prefix    = 'RAxML_bipartitions.';
-my $extension = 'nwk';
 my @colors    = qw(darkred darkviolet);
 GetOptions(
-	'verbose+'    => \$verbosity,
-	'genera'      => \$genera,
-	'intree=s'    => \$intree,
-	'dir=s'       => \$dir,
-	'width=i'     => \$width,
-	'height=i'    => \$height,
-	'format=s'    => \$format,
-	'shape=s'     => \$shape,
-	'mode=s'      => \$mode,
-	'prefix=s'    => \$prefix,
-	'extension=s' => \$extension,
-	'colors=s'    => sub { @colors = split /,/, pop }
+	'verbose+'  => \$verbosity,
+	'intree=s'  => \$intree,
+	'width=i'   => \$width,
+	'height=i'  => \$height,
+	'padding=i' => \$padding,
+	'shape=s'   => \$shape,
+	'mode=s'    => \$mode,
+	'taxa=s'    => \$taxa, # ML_trees_collapsed_families.tsv
+	'fossils=s' => \$fossils, # calibrations_timetree.tsv
+	'colors=s'  => sub { @colors = split /,/, pop }
 );
 
-# configure INFO/WARN/etc. messages 
+##########################################################################################
 Bio::Phylo::Util::Logger->new(
 	'-level' => $verbosity,
 	'-class' => 'main',
@@ -42,69 +38,52 @@ Bio::Phylo::Util::Logger->new(
 
 INFO "going to read tree $intree";
 my $tree = parse_tree(
-	'-format' => $format,
+	'-format' => 'nexus',
 	'-file'   => $intree,
 );
 
 INFO "going to create tree drawer";
 my $drawer = Bio::Phylo::Treedrawer->new(
-	'-format' => 'svg',
-	'-width'  => $width,
-	'-height' => $height,
-	'-shape'  => $shape,
-	'-mode'   => $mode,
-	'-tree'   => $tree,
+	'-format'  => 'svg',
+	'-width'   => $width,
+	'-height'  => $height,
+	'-shape'   => $shape,
+	'-mode'    => $mode,
+	'-tree'    => $tree,
+	'-padding' => $padding,
+	'-collapsed_clade_width' => 1/2,
+	'-text_horiz_offset'     => 10,
 );
 
 INFO "going to create color spectrum";
 my $spectrum = Color::Spectrum->new;
 
-INFO "going to scan clades directory $dir";
+##########################################################################################
+INFO "going to set clade labels";
 my $clades = 0;
-opendir my $dh, $dir or die $!;
-while( my $entry = readdir $dh ) {
-	if ( $entry =~ /$prefix(.+)\.$extension/ ) {
-		my $family = $1;
-		$family =~ s/_rooted$//g;
-		if ( $family =~ /[A-Z][a-z]+/ ) {
-
-
-			INFO "reading $dir/$entry for clade $family";
-			my $ft = parse_tree(
-				'-format' => 'newick',
-				'-file'   => "$dir/$entry",
-			);
-			my ($ingroup) = sort {
-				scalar @{ $b->get_terminals }
-				<=>
-				scalar @{ $a->get_terminals }
-			} @{ $ft->get_root->get_children };
-
-			INFO "matching ingroup tips with tree";
-			my @matches;
-			for my $tip ( @{ $ingroup->get_terminals } ) {
-				my $name = $tip->get_name;
-				if ( my $match = $tree->get_by_name($name) ) {
-
-					INFO "found match for $name";
-					push @matches, $match;
-				}
-				else {
-					DEBUG "no match for $name";
-				}
-			}
-
-			if ( @matches ) {
-
-				INFO "have ingroup matches, fetching mrca";
-				my $mrca = $tree->get_mrca(\@matches);
-				$mrca->set_clade_label($family);
+my %families;
+{
+	open my $fh, '<', $taxa or die $!;
+	while(<$fh>) {
+		chomp;
+		my ( $taxon, $family ) = split /\t/, $_;
+		if ( my $tip = $drawer->get_tree->get_by_name( $taxon ) ) {
+			if ( not $families{$family} ) {
+			
+				INFO "found family $family";
+				$families{$family} = [];
 				$clades++;
 			}
+			push @{ $families{$family} }, $tip;
 		}
+	}
+	for my $fam ( keys %families ) {
+		my $mrca = $drawer->get_tree->get_mrca( $families{$fam} );
+		$mrca->set_clade_label($fam);
 	}
 }
 
+##########################################################################################
 INFO "going to paint $clades clades with spectrum (@colors)";
 my @spectrum = $spectrum->generate( $clades, @colors );
 my $i = 0; # incrementing index
@@ -116,7 +95,8 @@ $tree->visit_depth_first(
 			INFO "going to paint $label with $spectrum[$i]";
 			$node->set_node_color($spectrum[$i]);
 			for my $d ( @{ $node->get_descendants } ) {
-				$d->set_node_color($spectrum[$i]);
+				#$d->set_node_color($spectrum[$i]);
+				$d->set_node_outline_color($spectrum[$i]);
 				$d->set_branch_color($spectrum[$i]);
 			}
 			$i++;
@@ -124,15 +104,18 @@ $tree->visit_depth_first(
 	}
 );
 
+##########################################################################################
 INFO "going to compute scale bar units";
 $drawer->get_tree->ladderize('reverse');
 $drawer->compute_coordinates;
 my $root = $drawer->get_tree->get_root;
-my $height_in_pixels = $root->get_leftmost_terminal->get_x - $root->get_x;
-my $height_in_myears = $root->calc_max_path_to_tips;
+my ($tallest) = sort { $b->get_x <=> $a->get_x } @{ $drawer->get_tree->get_entities };
+my $height_in_pixels = $tallest->get_x - $root->get_x;
+my $height_in_myears = $tallest->calc_path_to_root;
 my $pixels_per_myear = $height_in_pixels / $height_in_myears;
 
-INFO "setting scale, pixels per MYA: $pixels_per_myear";
+##########################################################################################
+INFO "setting scale, pixels per MYA: $height_in_pixels/$height_in_myears=$pixels_per_myear";
 $drawer->set_scale_options(
 	'-width'   => '100%',
 	'-major'   => $pixels_per_myear * 25,
@@ -151,10 +134,66 @@ $drawer->set_scale_options(
 	}
 );
 
+##########################################################################################
+INFO "going to collapse monophyletic genera";
+$drawer->get_tree->visit_depth_first(
+	'-post' => sub {
+		my $node = shift;
+		if ( $node->is_terminal ) {
+			my $name = $node->get_name;
+			if ( $name =~ /^([A-Z][a-z]+)/ ) {
+				my $genus = $1;
+				$node->set_generic( 'genus' => { $genus => 1 } );
+				DEBUG "$name belongs to genus $genus";
+			}
+		}
+		else {
+			my %genus;
+			for my $c ( @{ $node->get_children } ) {
+				my $g = $c->get_generic('genus');
+				for my $k ( keys %$g ) {
+					$genus{$k} = $g->{$k};
+				}
+			}
+			$node->set_generic( 'genus' => \%genus );
+			DEBUG "internal node subtends: ".Dumper(\%genus);
+		}
+	}
+);
+$drawer->get_tree->visit_depth_first(
+	'-pre' => sub {
+		my $node = shift;
+		if ( $node->is_internal ) {
+			my $g = $node->get_generic('genus');
+			
+			# node is poly/paraphyletic
+			if ( scalar(keys(%$g)) > 1 ) {
+				for my $c ( @{ $node->get_children } ) {
+					my $cg = $c->get_generic('genus');
+					
+					# child is a monophyletic internal nodes
+					if ( scalar(keys(%$cg)) == 1 and $c->is_internal ) {						
+						my $ntips = scalar @{ $c->get_terminals };
+						my ($genus) = keys %$cg;
+						my $name = "$genus (n=$ntips)";
+						$c->set_name($name);
+						$c->set_collapsed(1);
+						$c->set_font_size(11);
+						$c->set_font_face('Verdana');
+						$c->set_font_weight('bold');
+						INFO "found monophyletic node for $genus (n=$ntips)";
+					}
+				}
+			}
+		}
+	}
+);
+
+##########################################################################################
 INFO "writing SVG output to STDOUT";
 for my $tip ( @{ $drawer->get_tree->get_terminals } ) {
 	$tip->set_font_face('Verdana');
-	$tip->set_font_style('Italic');
 	$tip->set_font_size(11);
+	$tip->set_font_style('Italic') if not $tip->get_collapsed;	
 }
 print $drawer->draw;
